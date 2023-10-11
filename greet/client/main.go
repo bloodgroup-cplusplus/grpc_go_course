1b5eb64b16a38c4c3dbc93ffa05399c6ef67fa3b
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-10-11 13:44:00 +0530

    added grpc.WithTransportCredentials(insecure.NewCredentails()))

--- greet/client/main.go
+++ greet/client/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var addr string = "localhost:50051"
@@ -12,7 +13,7 @@ func main() {
 
 	// create a fucntion called grpc dial
 
-	conn, err := grpc.Dial(addr)
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
 		log.Fatalf("Failed to connect to :%v\n", err)
